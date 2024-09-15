7ba9073e23218e201121c431d5226eb67b46cc03
Mitchell Cohen <122713703+profmitchell@users.noreply.github.com>
2024-09-15 03:56:50 -0400

    Initial Commit

---
+++ fparse/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fparse
+//
+//  Created by Mitchell Cohen on 9/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
